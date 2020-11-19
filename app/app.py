1ca510250f119bb318463da2f5dc7751630de054
James Hasty <jameshasty22@gmail.com>
2020-11-19 14:38:26 -0500

    final flask app

--- app/app.py
+++ app/app.py
@@ -1,17 +1,34 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+#from flask_session import Session
+#SESSION_TYPE = 'memcache'
 import flask
 import pandas as pd
 import os
+from os import path
 import sys
 
 
 
 # Create an instance of Flask app
 app = Flask(__name__)
+#sess = Session()
+#my_resp = ""
 
+@app.route('/index')
 @app.route('/')
 def index():
-    return render_template('index3.html')
+    #response = request.args['response']
+    #response = session['response']
+    if path.exists("response.txt"):
+        file_r = open("response.txt","r+")
+        response = file_r.read()
+        file_r.close()
+        os.remove("response.txt")
+    else:
+        response = "Please Input Information"
+        print("no submit yet")
+
+    return render_template('index.html', response = response)
 
 import GPT2
 @app.route('/Date_Input', methods=["GET","POST"]) 
@@ -21,11 +38,12 @@ def Date_Input():
     height = request.form['height']
     score = request.form['score']
     birth = request.form['birth']
-    GPT2.Date_Input(sex,hair,height,score,birth)
-    response = request.form.get('response')
-    print("-----------------------------------------------")
-    print(response)
-    return render_template('index3.html', response = response) 
+    response = GPT2.Date_Input(sex,hair,height,score,birth)
+    #session['response'] = response
+    file_w = open("response.txt","w")
+    file_w.writelines(response)
+    file_w.close()
+    return redirect(url_for('.index')) 
    
 
 # @app.route('/my_resp', methods=['POST']) 
@@ -41,4 +59,8 @@ def Date_Input():
  
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
+    #app.secret_key = 'super secret key'
+    #app.config['SESSION_TYPE'] = 'filesystem'
+
+    #sess.init_app(app)
     app.run(debug = True)
